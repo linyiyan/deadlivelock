513cb0ffe0e94384fdd6ad6779ac21941231d64b
Yiyan Lin <linyiyan.msu@gmail.com>
2013-03-28 15:42:52 -0400

    change

--- main.ml
+++ main.ml
@@ -1,1 +1,17 @@
 open Cil
+open List
+open Cfg
+open Printf
+let f=Frontc.parse "deadlock.cil.c" ();;
+computeFileCFG f;;
+
+let isMutexGvar gv = 
+	match gv with
+	GVar(varinfo , initinfo , location) -> 	(match varinfo.vtype with											
+											| TNamed(typeinfo,_) -> true
+											| _ -> false)
+	|_ -> false;;
+
+let l=f.globals in
+let mutexl = List.filter (isMutexGvar) l in
+printf "%d\n" (List.length mutexl);;

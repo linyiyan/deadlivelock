39ccb37d5563f6a62d39d090cff19fa2cb8ea8fb
Yiyan Lin <linyiyan.msu@gmail.com>
2013-04-11 21:01:22 -0400

    simple dir

--- compile.sh
+++ compile.sh
@@ -7,4 +7,3 @@ cilly --save-temps -D HAPPY_MOOD $TARGET -lpthread
 #ocamlc -I $MYINCLUDE -I ./include/ocamlgraph/src -I ./include/ocamlgraph/lib -ccopt -L$MYINCLUDE -ccopt -L./include/ocamlgraph/src -ccopt -L./include/ocamlgraph/lib -c $MAINFILE.ml
 ocamlc -I $MYINCLUDE -I ../ocamlgraph/src -I ../ocamlgraph/lib -ccopt -L$MYINCLUDE -ccopt -L../ocamlgraph/src -ccopt -L../ocamlgraph/lib -c $MAINFILE.ml
 ocamlc -o $MAINFILE unix.cma str.cma nums.cma $MYINCLUDE/cil.cma ../ocamlgraph/graph.cma $MAINFILE.cmo
-

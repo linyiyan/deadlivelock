9d4c333e34921d36479761d79187ac20e7931b64
Yiyan Lin <linyiyan.msu@gmail.com>
2013-05-04 19:21:45 -0400

    test

---
+++ example/simple/test_trylock_while_livelock.c
@@ -0,0 +1,70 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+
+static int glob = 0;
+static pthread_mutex_t mtx1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mtx2 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mtx3 = PTHREAD_MUTEX_INITIALIZER;
+
+static void *                  
+threadFunc1(void *arg)
+{
+	int loop = (int)(*((int*)arg));
+	while(loop-- > 0){
+		init: while(pthread_mutex_trylock(&mtx3)!=0){}
+		pthread_mutex_lock(&mtx1);
+		
+		if(pthread_mutex_trylock(&mtx2)==0){
+			printf("in t1 %d\n" , glob++);
+			pthread_mutex_unlock(&mtx3);
+			pthread_mutex_unlock(&mtx1);
+			goto init;
+		}
+	}
+	return NULL;
+}
+
+static void *
+threadFunc2(void *arg)
+{
+	int loop = (int)(*((int*)arg));
+	while(loop-- > 0){
+		while(pthread_mutex_trylock(&mtx1)!=0){}
+		while(1){
+			if(pthread_mutex_trylock(&mtx2)==0){
+				if(pthread_mutex_trylock(&mtx3)==0){
+					printf("in t2 %d\n" , glob++);
+					pthread_mutex_unlock(&mtx3);
+					pthread_mutex_unlock(&mtx2);
+					pthread_mutex_unlock(&mtx1);
+					break;
+				}
+				else{
+					pthread_mutex_unlock(&mtx2);
+				}
+			}
+		}
+	}
+	
+	return NULL;
+}
+
+int main()
+{
+	pthread_t t1, t2;
+    int loops,s;
+	
+	loops = 10000;
+
+    s = pthread_create(&t1, NULL, threadFunc1, &loops);
+   
+    s = pthread_create(&t2, NULL, threadFunc2, &loops);
+  
+    s = pthread_join(t1, NULL);
+   
+    s = pthread_join(t2, NULL);
+  
+    printf("glob = %d\n", glob);
+    return 0;
+}
